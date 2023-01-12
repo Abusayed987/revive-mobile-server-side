a1940994c36d6d1548166d09659ab23e80bfa8d1
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2023-01-12 19:59:20 +0600

    create get categories functionalities

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 const express = require('express');
 const cors = require('cors');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 4000;
 
 // For dotenv 
-
+require('dotenv').config();
 
 
 // Middleware:
 app.use(cors())
 app.use(express.json())
 
+// for mongoDb doc 
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.dptn3ue.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+
+async function run() {
+    try {
+        const categoryCollection = client.db("ReviveMobile").collection("categoriesItems");
+
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const result = await categoryCollection.find(query).toArray();
+            res.send(result)
+        })
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await categoryCollection.findOne(query);
+            res.send(result)
+        })
+
+
+
+
+    }
+    finally {
+
+    }
+
+
+}
+run().catch(console.error);
 
 
 
