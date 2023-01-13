a7a959a8041fa7435310d88b33c722263f2e2934
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2023-01-13 11:05:47 +0600

    post all user and get all buyes and all seller user

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const categoryCollection = client.db("ReviveMobile").collection("categoriesItems");
+        const productsCollection = client.db("ReviveMobile").collection("products")
+        const allUserCollection = client.db("ReviveMobile").collection("allUser")
 
         app.get('/categories', async (req, res) => {
             const query = {}
@@ -38,6 +40,37 @@ async function run() {
             const result = await categoryCollection.findOne(query);
             res.send(result)
         })
+
+        app.post("/dashboard/seller/addProduct", async (req, res) => {
+            const product = req.body;
+            console.log(product);
+            // const result = await productsCollection.insertOne(product);
+            // res.send(result)
+        })
+
+        app.post("/dashboard/admin/allUser", async (req, res) => {
+            const userDetails = req.body;
+            const result = await allUserCollection.insertOne(userDetails);
+            res.send(result)
+        })
+
+        //admin er jonno all user get korte hobe 2 vabe...
+        //1. all buyers
+        app.get('/dashboard/admin/buyer', async (req, res) => {
+            const buyer = req.query.buyer;
+            const query = { role: buyer };
+            const allUser = await allUserCollection.find(query).toArray();
+            res.send(allUser)
+        })
+
+        //2. all sellers
+        app.get('/dashboard/admin/seller', async (req, res) => {
+            const seller = req.query.seller;
+            const query = { role: seller };
+            const allSeller = await allUserCollection.find(query).toArray();
+            res.send(allSeller);
+        })
+
 
 
 
